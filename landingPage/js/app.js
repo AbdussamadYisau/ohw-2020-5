21b49dd391221612b73a8e9b69665c71db4d0589
AbdussamadYisau <Sammieyisau@gmail.com>
2020-08-02 02:06:43 +0100

    Made few tweaks. Fixed Mobile responsiveness as well

--- landingPage/js/app.js
+++ landingPage/js/app.js
@@ -1,16 +1,14 @@
 const hamburger = document.getElementById('hamburger');
-const navBar = document.querySelector('.hide');
+const hamburgerSection = document.getElementById('hamburger-section');
 
 hamburger.addEventListener('click', displayMenu);
 
 function displayMenu(e) {
-    if (e.target.src.includes('id=1os-XVZj9O17KJ4BdoKrG4qAhKn6t7Kvh')) {
+    if (e.target.src.includes('https://res.cloudinary.com/dizrb3vql/image/upload/v1596145703/Group_twsno9.png')) {
         e.target.src = 'https://res.cloudinary.com/dizrb3vql/image/upload/v1596126035/close_pxrklj.svg';
-        // navBar.style.cssText = "display: flex;flex-direction: column;";
-        navBar.style.display = 'flex';
-        navBar.style.flexDirection = 'column-reverse';
+        hamburgerSection.style.display = 'flex';
     } else {
-        e.target.src = 'https://drive.google.com/uc?export=view&id=1os-XVZj9O17KJ4BdoKrG4qAhKn6t7Kvh';
-        navBar.style.display = 'none';
+        e.target.src = 'https://res.cloudinary.com/dizrb3vql/image/upload/v1596145703/Group_twsno9.png';
+        hamburgerSection.style.display = 'none';
     }
 }
